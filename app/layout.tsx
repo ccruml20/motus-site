0841300f6b1679955c1988d5bd78543a9352fd60
Chase Crumley <ccrumley1@gmail.com>
2026-02-11 18:08:53 -0500

    Add noindex protection for non-production environments

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,15 @@ import "./globals.css";
 import "./grain.css";
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  robots: {
+    index: process.env.VERCEL_ENV === "production",
+    follow: process.env.VERCEL_ENV === "production",
+  },
+};
+
 
 export default function RootLayout({
   children,
